3a9128af862aa0c5042535bb0622cfc32a567e95
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-04-17 05:25:45 +0600

    implement sign up from to create user

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SocialButton from "../SocialButton/SocialButton";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = event => {
+      setEmail(event.target.value);
+  }
   return (
     <div className="input-container">
       <div className="input-area">
         <form>
           <h3>Please Login</h3>
-          <input type="email" name="" id="" placeholder="Email" required />
+
+          <input onBlur={handleEmailBlur} type="email" name="" id="" placeholder="Email" required />
+
           <input
             type="password"
             name=""
@@ -17,15 +27,15 @@ const Login = () => {
             placeholder="Password"
             required
           />
+
           <button>Login</button>
         </form>
 
-    <p className="google-link m-4">New to this site? 
-    <Link to='/signup'>Create an Account</Link></p>
-    
+        <p className="google-link m-4">
+          New to this site?
+          <Link to="/signup">Create an Account</Link>
+        </p>
 
-
-   
         <SocialButton></SocialButton>
       </div>
     </div>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,28 +1,85 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import SocialButton from "../SocialButton/SocialButton";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+if(user){
+    navigate('/')
+}
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Your password did not match");
+    }
+    if (password.length < 6) {
+      setError("Password must be 6 characters or longer");
+    }
+
+    createUserWithEmailAndPassword(email, password);
+  };
+
+
+
+
   return (
     <div className="input-container">
       <div className="input-area">
-        <form>
+        <form onSubmit={handleCreateUser}>
           <h3>Please Sign up</h3>
-          <input type="email" name="" id="" placeholder="Email" required />
+
           <input
+            onBlur={handleEmailBlur}
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+            required
+          />
+
+          <input
+            onBlur={handlePassword}
             type="password"
             name=""
             id=""
             placeholder="Password"
             required
           />
+
           <input
+            onBlur={handleConfirmPassword}
             type="password"
             name=""
             id=""
             placeholder="Confirm Password"
             required
           />
+
+          <p style={{ color: "red" }}>{error}</p>
+
           <button>Sign up</button>
         </form>
 

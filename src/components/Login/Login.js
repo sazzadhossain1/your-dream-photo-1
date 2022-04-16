32b1007c0a1d2bc4faf6c7e1ae23c6412c559f6c
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-04-17 05:44:24 +0600

    sign in user done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,58 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import SocialButton from "../SocialButton/SocialButton";
 import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
 
-  const handleEmailBlur = event => {
-      setEmail(event.target.value);
+  const navigate = useNavigate();
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleUserSignIn = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+  if (user) {
+    navigate("/");
   }
   return (
     <div className="input-container">
       <div className="input-area">
-        <form>
+        <form onSubmit={handleUserSignIn}>
           <h3>Please Login</h3>
 
-          <input onBlur={handleEmailBlur} type="email" name="" id="" placeholder="Email" required />
+          <input
+            onBlur={handleEmailBlur}
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+            required
+          />
 
           <input
+            onBlur={handlePassword}
             type="password"
             name=""
             id=""
             placeholder="Password"
             required
           />
-
+          {loading && <p>Loading...</p>}
+          <p style={{ color: "red" }}>{error?.message}</p>
           <button>Login</button>
         </form>
 

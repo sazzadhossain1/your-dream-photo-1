69d3ec6db1badb14605401ad9ed1d7a8f097bb60
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-04-17 07:30:14 +0600

    google sign in button impliment done

--- src/components/SocialButton/SocialButton.js
+++ src/components/SocialButton/SocialButton.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./SocialButton.css";
-import googleLogo from '../../images/google.png';
+import googleLogo from "../../images/google.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const SocialButton = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorGoogle;
+
+  if (error) {
+    errorGoogle = (
+      <div>
+        <p className="text-danger">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    navigate("/");
+  }
   return (
     <div>
-      <button className="google-button">
+      <div className="parent-line">
+        <div className="line"></div>
+        <p className="or">or</p>
+        <div className="line"></div>
+        
+      </div>
+      {errorGoogle}
+      <button onClick={() => signInWithGoogle()} className="google-button">
         <img src={googleLogo} alt="" /> <p>Countinue With Google</p>
       </button>
     </div>
